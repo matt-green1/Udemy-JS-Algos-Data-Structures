a94f99b06aac9c7443a78a91dd59ae3478a37409
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2021-02-02 15:15:11 -0500

    insertion sort and merge sort

---
+++ 87 - merge sort.js
@@ -0,0 +1,32 @@
+// this just covers how we write teh code to merge two sorted arrays
+
+// Merges two already sorted arrays
+function merge(arr1, arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++;
+        }
+    }
+
+    //the below while loops catch the cases where one of the arrays is "exhausted"
+    
+    while(i < arr1.length) {
+        results.push(arr1[i])
+        i++;
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    return results;
+}
+merge([100,200], [1,2,3,5,6])
+
+                   
