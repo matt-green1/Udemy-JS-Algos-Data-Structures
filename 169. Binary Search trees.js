f7f0180361dadf1061d826dc4df7fe25a423d25b
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2021-02-16 10:40:33 -0500

    binary search and started tree traversal

---
+++ 169. Binary Search trees.js
@@ -0,0 +1,99 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        //this is a temp variable which tracks the parent going down tree
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }
+    }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        //we have current in there because if we never find the value
+        // found will always be false
+        // if we try to set current to a value that doesn't exist (null) it will be false and we'll break out
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        
+        //we only get here after we haven't found the value int he tree
+        
+        // so found will always be false and we can return undefined
+        if(!found) return undefined;
+        
+        //if found is TRUE so the above if statement doesn't run
+        // we return the found node
+        return current;
+    }
+
+    // this is an implementation that returns true/false as opposed to returning the node itself
+    contains(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+//      10
+//   5     13
+// 2  7  11  16
+
+var tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+
+
+
+
