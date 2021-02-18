5d64b9690d760eab434dab3d169a658e96cd32a6
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2021-02-18 12:01:03 -0500

    heaps continued

--- 186 Heap insert and extract max.js
+++ 186 Heap insert and extract max.js
@@ -2,20 +2,42 @@ class MaxBinaryHeap {
     constructor(){
         this.values = [];
     }
-    //Thurs - start by walking through this code in your head
+
     insert(element){
+        //we start by adding new element onto end of array
         this.values.push(element);
         this.bubbleUp();
     }
     bubbleUp(){
+        //This line finds the last element in the array
+        //We'll update this this variable as the element "bubbles up"  to record its new index
         let idx = this.values.length - 1;
+        
+        //this uses the above index to STORE the value of the last element itself (that we just added)
         const element = this.values[idx];
+        //alt code to the above line: just have the helper method "bubble up" accept the element from outer function insert
+
+        //this condition makes the bubbling up stop IF we get to the root
         while(idx > 0){
+            //finds parent index using the formula for doing so from a child
             let parentIdx = Math.floor((idx - 1)/2);
+            
+            //stores parent value - not neccessary but a little cleaner - could alternatively just
+            // compare what's on the right side of the equals sign
             let parent = this.values[parentIdx];
+            
+            // break out of while loop if the element is NOT bigger than parent
+            //so it will stop bubbling where it is
             if(element <= parent) break;
+            
+            //assuming we DON'T break and we need to bubble...
+            //we set the parent index to the value of the element
             this.values[parentIdx] = element;
+
+            //we set the value of the added element to the formerly parent value 
             this.values[idx] = parent;
+            
+            //we set the indx up top to the formely parent index (completing the bubble up)
             idx = parentIdx;
         }
     }
